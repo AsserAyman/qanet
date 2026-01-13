29a2ca23be62dd21fcb61770b1079a2771744af0
Asser Ayman <asser.ayman@gmail.com>
2026-01-13 21:04:13 +0200

    RTL tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,36 @@ import { Platform } from 'react-native';
 import { useI18n } from '../../contexts/I18nContext';
 
 export default function TabLayout() {
-  const { t } = useI18n();
+  const { t, isRTL } = useI18n();
+
+  const tabs = [
+    {
+      name: 'index',
+      label: t('nightPrayer'),
+      iosIcon: { default: 'moon', selected: 'moon.fill' },
+      androidIcon: { family: Feather, name: 'moon' },
+    },
+    {
+      name: 'add',
+      label: t('addPrayer'),
+      iosIcon: { default: 'plus.circle', selected: 'plus.circle.fill' },
+      androidIcon: { family: Feather, name: 'plus-circle' },
+    },
+    {
+      name: 'history',
+      label: t('history'),
+      iosIcon: { default: 'clock', selected: 'clock.fill' },
+      androidIcon: { family: MaterialIcons, name: 'history' },
+    },
+    {
+      name: 'settings',
+      label: t('settings'),
+      iosIcon: { default: 'gearshape', selected: 'gearshape.fill' },
+      androidIcon: { family: Feather, name: 'settings' },
+    },
+  ];
+
+  const orderedTabs = isRTL ? [...tabs].reverse() : tabs;
 
   return (
     <NativeTabs
@@ -15,37 +44,15 @@ export default function TabLayout() {
       // minimizeBehavior="onScrollDown"
       disableTransparentOnScrollEdge={false}  // Allows transparency
     >
-      <NativeTabs.Trigger name="index">
-        <Label>{t('nightPrayer')}</Label>
-        {Platform.select({
-          ios: <Icon sf={{ default: 'moon', selected: 'moon.fill' }} />,
-          android: <Icon src={<VectorIcon family={Feather} name="moon" />} />,
-        })}
-      </NativeTabs.Trigger>
-
-      <NativeTabs.Trigger name="add">
-        <Label>{t('addPrayer')}</Label>
-        {Platform.select({
-          ios: <Icon sf={{ default: 'plus.circle', selected: 'plus.circle.fill' }} />,
-          android: <Icon src={<VectorIcon family={Feather} name="plus-circle" />} />,
-        })}
-      </NativeTabs.Trigger>
-
-      <NativeTabs.Trigger name="history">
-        <Label>{t('history')}</Label>
-        {Platform.select({
-          ios: <Icon sf={{ default: 'clock', selected: 'clock.fill' }} />,
-          android: <Icon src={<VectorIcon family={MaterialIcons} name="history" />} />,
-        })}
-      </NativeTabs.Trigger>
-
-      <NativeTabs.Trigger name="settings">
-        <Label>{t('settings')}</Label>
-        {Platform.select({
-          ios: <Icon sf={{ default: 'gearshape', selected: 'gearshape.fill' }} />,
-          android: <Icon src={<VectorIcon family={Feather} name="settings" />} />,
-        })}
-      </NativeTabs.Trigger>
+      {orderedTabs.map((tab) => (
+        <NativeTabs.Trigger key={tab.name} name={tab.name}>
+          <Label>{tab.label}</Label>
+          {Platform.select({
+            ios: <Icon sf={tab.iosIcon} />,
+            android: <Icon src={<VectorIcon family={tab.androidIcon.family} name={tab.androidIcon.name} />} />,
+          })}
+        </NativeTabs.Trigger>
+      ))}
     </NativeTabs>
   );
 }
